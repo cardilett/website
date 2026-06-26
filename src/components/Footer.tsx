21ad5f0af6a145eab774475154cffd0f994a47ef
Tommy Milkov <81772473+tommymilkov@users.noreply.github.com>
2026-06-26 14:22:07 +0400

    Contact time change

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -72,7 +72,7 @@ export default function Footer() {
                 </a>
               </li>
               <li>Abu Dhabi, United Arab Emirates</li>
-              <li>Sun–Thu · 08:00–16:00 UAE time</li>
+              <li>Mon-Fri · 08:00–16:00 UAE time</li>
             </ul>
           </div>
         </div>
